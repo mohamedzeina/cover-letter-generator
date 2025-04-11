23388390464e4b9aa27f5942f15523991b278ffc
Mohamed Zeina <toxiczeina@gmail.com>
2025-04-12 00:16:09 +0300

    added function to extract the text from the pdf or docx file uploaded

--- coverLetterGen.py
+++ coverLetterGen.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pdfplumber
+import docx2txt
 import io
+import logging as log
+import os
+
+def extractText(file):
+    if file.type == "application/pdf":
+        with pdfplumber.open(file) as pdf:
+            return "\n".join(page.extract_text() for page in pdf.pages if page.extract_text())
+    elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        return docx2txt.process(file)
+    else:
+        return "Unsupported file type."
 
 
 
@@ -13,9 +26,9 @@ jobDesc = st.text_area("Paste the Job Description")
 
 if st.button("Generate Cover Letter"):
     if uploadedFile and jobTitle and companyName and jobDesc:
-        #resume_text = extract_text(uploaded_file)
+        resumeText = extractText(uploadedFile)
         #cover_letter = generate_cover_letter(resume_text, job_title, company_name, job_description)
         st.subheader("📬 Your Cover Letter")
-        st.text_area("Generated Letter", "cover_letter", height=300)
+        st.text_area("Generated Letter", resumeText, height=300)
     else:
         st.error("Please fill in all fields and upload your resume.")
